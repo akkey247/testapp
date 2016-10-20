9c1222b4f0b15ab2d40151d3f5a9772a39ab96c2
akkey247 <akkey52417@gmail.com>
2016-10-20 21:17:14 +0900

    ajax通信用ソース修正

--- ajax.php
+++ ajax.php
@@ -4,57 +4,33 @@
 
 <script type="text/javascript">
 <!--
-jQuery( function() {
-    jQuery( '#jquery-sample-button' ) . toggle(
-        function() {
-            jQuery . ajax( {
-                url: 'jquery-sample-ajax-php.php',
-                data: {
-                    year: '2011',
-                    month: '11',
-                    day: '24'
-                },
-                success: function( data ) {
-                    jQuery( '#jquery-sample-ajax' ) . html( data );
-                    jQuery( '#jquery-sample-textStatus' ) . text( '読み込み成功' );
-                },
-                error: function( data ) {
-                    jQuery( '#jquery-sample-textStatus' ) . text( '読み込み失敗' );
-                }
-            } );
-        },
-        function() {
-            jQuery( '#jquery-sample-ajax' ) . html( '' );
-            jQuery( '#jquery-sample-textStatus' ) . text( '' );
-        }
-    );
-} );
+$( function() {
+  $('#ajax-button').onclick(function(){
+    $.ajax( {
+      url: 'jquery-sample-ajax-php.php',
+      data: {
+        year: '2011',
+        month: '11',
+        day: '24'
+      },
+      success: function( data ) {
+        $('#result-box').html(data);
+        $('#connection-status').text('読み込み成功');
+      },
+      error: function( data ) {
+        $('#connection-status').text('読み込み失敗');
+      }
+    });
+  });
+});
 // -->
 </script>
-<style type="text/css">
-<!--
-#jquery-sample {
-    margin: 10px;
-    width: 200px;
-}
-#jquery-sample-ajax {
-    margin: 10px;
-    padding: 10px;
-    height: 100px;
-    background-color: yellow;
-    border: 1px solid gray;
-    border-radius: 10px;
-}
--->
-</style>
 </head>
 <body>
-<div id="jquery-sample">
-    <p>
-        <button id="jquery-sample-button">toggle</button>
-        <span id="jquery-sample-textStatus"></span>
-    </p>
-    <div id="jquery-sample-ajax"></div>
-</div>
+  <p>
+    <button id="ajax-button">クリック</button>
+    <span id="connection-status"></span>
+  </p>
+  <div id="result-box"></div>
 </body>
 </html>
