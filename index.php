9907b085f6b9ea92183b9e55f3300df923af352b
akkey247 <akkey52417@gmail.com>
2016-10-28 22:07:17 +0900

    目次用ページの表示を調整

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <head><title>目次</title></head>
 <body>
 <h1>目次</h1>
-<a href="/dbconnect.php">普通にDB接続結果を取得</a>
-<a href="/ajax.php">Ajax通信でDB接続結果を取得</a>
+<ul>
+  <li><a href="/dbconnect.php">普通にDB接続結果を取得</a></li>
+  <li><a href="/ajax.php">Ajax通信でDB接続結果を取得</a></li>
 </body>
 </html>
