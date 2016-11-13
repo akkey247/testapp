78f113c9a33797829a285944ec0a89669743a56e
akkey247 <akkey52417@gmail.com>
2016-11-13 16:49:56 +0900

    スマホフリフリカウント

---
+++ js/sp.js
@@ -0,0 +1,35 @@
+//自分のappIDを入力
+var milkcocoa = new MilkCocoa('hotivgbc6pr.mlkcca.com');
+var ds = milkcocoa.dataStore('countdata');
+
+var count = 0;
+var countbox = document.getElementById('count');
+var nameform = document.getElementById('nameform');
+
+
+var shakecheck = function(name) {
+  $(this).gShake(function() {
+    count++;
+    countbox.innerHTML = 'count: '+ count;
+    data ={};
+    data.name = name;
+    data.count = count;
+    ds.send({data: data});
+  });
+}
+
+function namefunc(){
+    name = document.nameform.nameval.value;
+    nameform.innerHTML = "your name " + name;
+    shakecheck(name);
+}
+
+
+
+//改行でsubmitさせないようにする
+function submitStop(e){
+    if (!e) var e = window.event;
+
+    if(e.keyCode == 13)
+        return false;
+}
