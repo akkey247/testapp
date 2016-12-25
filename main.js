073cffdd52ab273a0faee3bfa02904ceebf1cc5f
akkey247 <akkey52417@gmail.com>
2016-12-25 12:34:43 +0900

    パスによって表示を切り替え

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World')
+})
+
+app.get('/test', function (req, res) {
+  res.send('Test')
 })
 
 app.listen(process.env.PORT || 8080)
