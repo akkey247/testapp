451082a7c3475cf901645fa6d7308fe647fb2f2e
akkey247 <akkey52417@gmail.com>
2016-12-26 21:57:34 +0900

    自作モジュールの呼び出しに挑戦

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-var express = require('express')
+var express = require('express') //expressモジュール呼び出し
+var subfunc = require('test-module') //自分で作ったテストモジュールの呼び出し
+
 var app = express()
 
+// `/hello` にアクセスした際に「Hello World」と表示
 app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
 
+// `/test` にアクセスした際に「test」と表示
 app.get('/test', function (req, res) {
-  res.send('Test')
+  res.send(subfunc())
 })
 
 app.listen(process.env.PORT || 8080)
